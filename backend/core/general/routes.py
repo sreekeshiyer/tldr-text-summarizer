dd4553cc0d962d7a74089fc6b2c58eb89abba1d3
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-03-22 19:58:00 +0530

    Changed Requirements

--- backend/core/general/routes.py
+++ backend/core/general/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, Response
-from auth import demo_guard
+from auth import demo_guard, main_guard
 from config import BASE_URL
 
 general = Blueprint("general", __name__)
@@ -19,6 +19,56 @@ def hello():
 def auth_check():
     return Response(
         """{"greeting": "Hello. Welcome to the API. You have Authenticated Access."}""",
+        status=200,
+        mimetype="application/json",
+    )
+
+
+@general.route(f"{BASE_URL}/demo")
+@demo_guard
+def demo():
+    return Response(
+        """{"greeting": "Hello. Welcome to the API. You have Authenticated Access."}""",
+        status=200,
+        mimetype="application/json",
+    )
+
+
+@general.route(f"{BASE_URL}/summarize_from_text")
+@main_guard
+def summary_from_text():
+    # TODO : Call summarize_from_text function (utils.py) here
+    # Replace static strings with the tuple values you get in return
+    res = {"result_text": "result text"}
+    return Response(
+        res,
+        status=200,
+        mimetype="application/json",
+    )
+
+
+@general.route(f"{BASE_URL}/summarize_from_url")
+@main_guard
+def summary_from_url():
+    # TODO : Call summarize_from_url function (utils.py) here
+    # Replace static strings with the tuple values you get in return
+    res = {"extracted_text": "extracted_text", "result_text": "summary"}
+    return Response(
+        res,
+        status=200,
+        mimetype="application/json",
+    )
+
+
+@general.route(f"{BASE_URL}/summarize_from_file")
+@main_guard
+def summary_from_url():
+    # TODO : Call summarize_from_file function (utils.py) here
+    # Replace static strings with the tuple values you get in return
+    # Validation is required here, I'll add it later
+    res = {"extracted_text": "extracted_text_from_file", "result_text": "summary"}
+    return Response(
+        res,
         status=200,
         mimetype="application/json",
     )
--- backend/core/general/utils.py
+++ backend/core/general/utils.py
@@ -1,1 +1,51 @@
+import textract
+import os
+from werkzeug.utils import secure_filename
+
+# Constants
+
+SAVE_DIR = "./static/temp/"
+ALLOWED_EXTENSIONS = {"txt", "pdf", "doc", "docx"}
+
 # Utility Functions
+
+
+def allowed_file(filename: str):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def extract_text_from_file(file):
+
+    if file and allowed_file(file.filename):
+
+        filename = secure_filename(file.filename)
+
+        file.save(os.path.join(SAVE_DIR, filename))
+
+        currFile = os.path.join(SAVE_DIR) + filename
+
+        text = textract.process(currFile)
+
+        return {"text": text, "filename": filename}
+
+
+# TODO 1: Make a Utility function from your model, import it here, such that, you pass plain text as argument and get the summarized text in return. Preferably create a new file models.py in the same directory to do that.
+
+
+def summarize_from_url(url: str) -> tuple(str):
+
+    # TODO 2: Call another Utility Function (you'll have to create one) to extract text from a URL.
+    # Eg. text = 'extracted_text_from_url'
+
+    # TODO 3: Call the Main Function, pass in the text as argument
+    # Eg. return summarize(text)
+    return (url, "extracted_text" "summarized_text_from_url")
+
+
+def summarize_from_file(file: str) -> tuple(str):
+
+    # TODO 4: Call extract_text_from_file to get the extracted text.
+
+    # TODO 5: Call the Main Function, pass in the text as argument
+    # Eg. return summarize(text)
+    return ("extracted_text_from_file", "summarized_text")
